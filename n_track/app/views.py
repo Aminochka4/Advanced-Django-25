73d2e31a5da13a69deaae1055fefcbe78b7d9bbc
Amina Amanzholova <eomino1607@gmail.com>
2025-02-24 18:34:59 +0500

    n_tracker

--- n_track/app/views.py
+++ n_track/app/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import Food, Consume
-
+from .models import Food, Consume, HealthGoal, Food
+from django.contrib.auth.decorators import login_required
+from .forms import HealthGoalForm, FoodForm
+from django.http import JsonResponse 
+from django.contrib.auth.forms import UserCreationForm 
 
 def index(request): 
-    if request.method =="POST":
+    if request.method == "POST":
         food_consumed = request.POST['food_consumed']
         c = Food.objects.get(name=food_consumed)
         user = request.user
         consume = Consume(user=user, food_consumed=c)
         consume.save()
-        foods = Food.objects.all()
-    else:
-        foods = Food.objects.all()
+
+    foods = Food.objects.all()
     consumed_food = Consume.objects.filter(user=request.user)
-    return render(request, 'app/index.html', {'foods': foods, 'consumed_food':consumed_food}) 
+    health_goal, created = HealthGoal.objects.get_or_create(user=request.user)
+
+    return render(request, 'app/index.html', {
+        'foods': foods,
+        'consumed_food': consumed_food,
+        'health_goal': health_goal
+    })
 
 def delete_consume(request, id):
     consumed_food = Consume.objects.get(id=id)
@@ -22,3 +30,47 @@ def delete_consume(request, id):
         return redirect('/')
     return render(request, 'app/delete.html')
 
+@login_required
+def update_goal(request):
+    goal, created = HealthGoal.objects.get_or_create(user=request.user)
+
+    if request.method == "POST":
+        form = HealthGoalForm(request.POST, instance=goal)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = HealthGoalForm(instance=goal)
+
+    return render(request, 'app/update_goal.html', {'form': form})
+
+def chart_data(request): 
+    consumed = Consume.objects.filter(user=request.user) 
+    data = { 
+        "labels": [c.food_consumed.name for c in consumed], 
+        "carbs": [c.food_consumed.carbs for c in consumed], 
+        "proteins": [c.food_consumed.proteins for c in consumed], 
+        "fats": [c.food_consumed.fats for c in consumed], 
+        "calorie": [c.food_consumed.calorie for c in consumed], 
+    } 
+    return JsonResponse(data) 
+
+def register(request): 
+    if request.method == "POST": 
+        form = UserCreationForm(request.POST) 
+        if form.is_valid(): 
+            form.save() 
+            return redirect("login") 
+    else: 
+        form = UserCreationForm() 
+    return render(request, "app/register.html", {"form": form}) 
+
+def add_food(request): 
+    if request.method == "POST": 
+        form = FoodForm(request.POST) 
+        if form.is_valid(): 
+            form.save()  # Saves the food to the global registry 
+            return redirect("index")  # Redirects back to the homepage 
+    else: 
+        form = FoodForm() 
+    return render(request, "app/add_food.html", {"form": form}) 
--- n_track/n_track/urls.py
+++ n_track/n_track/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('delete/<int:id>/', views.delete_consume, name="delete"),
-    
+    path('update-goal/', views.update_goal, name='update_goal'),
+    path("chart-data/", views.chart_data, name="chart-data"), 
+    path("register/", views.register, name="register"), 
+    path("login/", auth_views.LoginView.as_view(template_name="app/login.html"), name="login"), 
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"), 
+    path("add-food/", views.add_food, name="add-food"), 
 ]
